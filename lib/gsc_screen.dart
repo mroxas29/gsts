8f733776266bdf3a64c409fb30092799c940b303
Michael Roxas <mroxas29@gmail.com>
2023-10-03 15:45:27 +0800

    Added marion

--- lib/gsc_screen.dart
+++ lib/gsc_screen.dart
@@ -3,8 +3,9 @@ import 'package:side_navigation/side_navigation.dart';
 import 'package:sysadmindb/main.dart';
 
 void main() {
-  runApp(MaterialApp(home: Gscscreen()),
-   );
+  runApp(
+    MaterialApp(home: Gscscreen()),
+  );
 }
 
 class Gscscreen extends StatefulWidget {
@@ -15,38 +16,45 @@ class Gscscreen extends StatefulWidget {
 }
 
 class _MainViewState extends State<Gscscreen> {
-  
-  
   /// Views to display
   List<Widget> views = const [
     Center(
       child: Text(
-        'Dashboard', 
+        'Dashboard',
         textDirection: TextDirection.ltr,
-        style: TextStyle(fontFamily: 'Inter'),),
-      
+        style: TextStyle(fontFamily: 'Inter'),
+      ),
     ),
     Center(
-      child: Text('Program Management',
-      textDirection:TextDirection.ltr,
-      style: TextStyle(fontFamily: 'Inter', ),),
+      child: Text(
+        'Program Management',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontFamily: 'Inter',
+        ),
+      ),
     ),
     Center(
-      child: Text('Student Management',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontFamily: 'Inter', fontSize: 100),),
+      child: Text(
+        'Student Management',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontFamily: 'Inter', fontSize: 100),
+      ),
     ),
     Center(
-      child: Text('Calendar',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontFamily: 'Inter', fontSize: 100),),
+      child: Text(
+        'Calendar',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontFamily: 'Inter', fontSize: 100),
+      ),
     ),
     Center(
-      child: Text('Inbox',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontFamily: 'Inter', fontSize: 100),),
+      child: Text(
+        'Inbox',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontFamily: 'Inter', fontSize: 100),
+      ),
     ),
-    
   ];
 
   /// The currently selected index of the bar
@@ -54,119 +62,109 @@ class _MainViewState extends State<Gscscreen> {
 
   @override
   Widget build(BuildContext context) {
-    
-    return WillPopScope( 
+    return WillPopScope(
       onWillPop: () async => false,
-      child: 
-      
-       
-    
-    Scaffold(
-      // The row is needed to display the current view
+      child: Scaffold(
+        // The row is needed to display the current view
 
-      body: Row( 
-        children: [
-          /// Pretty similar to the BottomNavigationBar!
-          SideNavigationBar(
-            
-            header: SideNavigationBarHeader( 
-              image: CircleAvatar(),
-              title: Text('Graduate school coordinator', style: TextStyle(color: Colors.white, fontSize: 16),),
-              subtitle: Text('marion_paguia@dlsu.edu.ph',style: TextStyle(color: Color(0xFF747475), fontSize: 12,),)
-            ),
-            
-            
-            
-            footer: SideNavigationBarFooter(
-            label: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                
-              ElevatedButton.icon(
-                 icon: Icon(Icons.logout, color:Color(0xFF747475) ,), 
-                label: Text('Log Out', style: TextStyle(color:Color(0xFF747475) ),),
-                style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
- 
-                
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const FirstRoute()),
-                );
-              },
-
-
-            ),
-
-            ],)
-            ),
-
-            
-            
-            selectedIndex: selectedIndex,
-            items: const [
-             
-              SideNavigationBarItem(
-                icon: Icons.dashboard,
-                label:'Dashboard',
-              ),
-              SideNavigationBarItem(
-                icon: Icons.book,
-                label: 'Program Management',
-              ),
-              SideNavigationBarItem(
-                icon: Icons.school,
-                label: 'Student Management',
-              ),
-              SideNavigationBarItem(
-                icon: Icons.event,
-                label: 'Calendar',
-              ),
-    
-              SideNavigationBarItem(
-                icon: Icons.inbox,
-                label: 'Inbox',
-              ),
-            ],
-            onTap: (index) {
+        body: Row(
+          children: [
+            /// Pretty similar to the BottomNavigationBar!
+            SideNavigationBar(
+              header: SideNavigationBarHeader(
+                  image: CircleAvatar(),
+                  title: Text(
+                    'Graduate school coordinator',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  subtitle: Text(
+                    'marion_paguia@dlsu.edu.ph',
+                    style: TextStyle(
+                      color: Color(0xFF747475),
+                      fontSize: 12,
+                    ),
+                  )),
+              footer: SideNavigationBarFooter(
+                  label: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(
+                      Icons.logout,
+                      color: Color(0xFF747475),
+                    ),
+                    label: Text(
+                      'Log Out',
+                      style: TextStyle(color: Color(0xFF747475)),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FirstRoute()),
+                      );
+                    },
+                  ),
+                ],
+              )),
+              selectedIndex: selectedIndex,
+              items: const [
+                SideNavigationBarItem(
+                  icon: Icons.dashboard,
+                  label: 'Dashboard',
+                ),
+                SideNavigationBarItem(
+                  icon: Icons.book,
+                  label: 'Program Management',
+                ),
+                SideNavigationBarItem(
+                  icon: Icons.school,
+                  label: 'Student Management',
+                ),
+                SideNavigationBarItem(
+                  icon: Icons.event,
+                  label: 'Calendar',
+                ),
+                SideNavigationBarItem(
+                  icon: Icons.inbox,
+                  label: 'Inbox',
+                ),
+              ],
+              onTap: (index) {
                 setState(() {
-                selectedIndex = index;
-              });
-            },
-             toggler: SideBarToggler(
-                expandIcon: Icons.keyboard_arrow_right,
-                shrinkIcon: Icons.keyboard_arrow_left,
-                onToggle: () {
-                  print('Toggle');
-                }),
-
-            
-            theme: SideNavigationBarTheme(
-              
-              itemTheme: SideNavigationBarItemTheme(
-                labelTextStyle: TextStyle(fontFamily: 'Inter', fontSize:14),
-                unselectedItemColor: Color(0xFF747475),
-                selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
-                iconSize: 20,
+                  selectedIndex = index;
+                });
+              },
+              toggler: SideBarToggler(
+                  expandIcon: Icons.keyboard_arrow_right,
+                  shrinkIcon: Icons.keyboard_arrow_left,
+                  onToggle: () {
+                    print('Toggle');
+                  }),
+              theme: SideNavigationBarTheme(
+                itemTheme: SideNavigationBarItemTheme(
+                  labelTextStyle: TextStyle(fontFamily: 'Inter', fontSize: 14),
+                  unselectedItemColor: Color(0xFF747475),
+                  selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+                  iconSize: 20,
+                ),
+                backgroundColor: Color(0xF0151718),
+                togglerTheme: SideNavigationBarTogglerTheme(
+                    expandIconColor: Colors.white,
+                    shrinkIconColor: Colors.white),
+                dividerTheme: SideNavigationBarDividerTheme.standard(),
               ),
-              backgroundColor: Color(0xF0151718),
-              togglerTheme: SideNavigationBarTogglerTheme(expandIconColor:Colors.white, shrinkIconColor: Colors.white),
-              dividerTheme: SideNavigationBarDividerTheme.standard(),
-              
             ),
-          ),
 
-          Expanded(
-            child: views.elementAt(selectedIndex),
-          )
-
-        ],
-        
-        
+            Expanded(
+              child: views.elementAt(selectedIndex),
+            )
+          ],
+        ),
       ),
-    )
-    ,) ;
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,136 +4,123 @@ import 'package:sysadmindb/gsc_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-void main() async{
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(
     title: 'Login Screen',
     home: FirstRoute(),
   ));
-
-  
 }
 
+//hello marion
 class FirstRoute extends StatelessWidget {
   const FirstRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-   
-   
     return WillPopScope(
-    onWillPop: () async => false,
-    child:  
-    
-    Scaffold(
- 
-      appBar: AppBar(
-        title: const Text('Graduate student tracking system'),
-        backgroundColor: Color.fromRGBO(18, 128, 86, 100),
-      ),
-      body: Container(
-       
-        decoration: BoxDecoration(
-         image: DecorationImage(image: AssetImage("assets/images/bg.png"), fit: BoxFit.cover)
-        ),
-        child:Center(
-        
-        child: SizedBox(
-        
-        width: 400,
-        height: 400,
-        child: Container(
-       
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Color.fromRGBO(31, 47, 41, 0.711),
-
-        ),
-
-        child:Column(
-          children: [
-                    SizedBox(height: 20),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 1),
-            
-            child: Text('Login',
-            style: TextStyle(fontSize: 40,
-            fontFamily: 'inter',color: Colors.white),textAlign: TextAlign.justify,),
+      onWillPop: () async => false,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Graduate student tracking system'),
+            backgroundColor: Color.fromRGBO(18, 128, 86, 100),
           ),
-
-          Padding(padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-
-            child: TextField(
-              
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'ID Number',
-                hintText: 'Enter ID number here',
-                floatingLabelAlignment: FloatingLabelAlignment.start,
-                floatingLabelStyle: TextStyle(fontSize: 25,),
-                filled: true,
-                fillColor: Colors.white,
-              ),
-            ),
-          
-          ),
-
-         Padding(padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                hintText: 'Enter password here',
-                filled: true,
-                fillColor: Colors.white,
-              ),
-            ),
-          
-          ),
-
-          Padding(padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-          
-          child:  
-              ElevatedButton(
-              child: const Text('Coordinator Screen'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Gscscreen()),
-                );
-              },
-            ),
-          
-          ),
-     
-        Padding(padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-          child:          
-                ElevatedButton(
-                child: const Text('Graduate Student Screen'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const GradStudentscreen()),
-                  );
-                },
-              ), 
-          ),
-
-        ],) 
-
-    
-      ),
-
-      ) ,
-      )
-      )
-    )
-    ,);
+          body: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/bg.png"),
+                      fit: BoxFit.cover)),
+              child: Center(
+                child: SizedBox(
+                  width: 400,
+                  height: 400,
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromRGBO(31, 47, 41, 0.711),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 1),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  fontFamily: 'inter',
+                                  color: Colors.white),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'ID Number',
+                                hintText: 'Enter ID number here',
+                                floatingLabelAlignment:
+                                    FloatingLabelAlignment.start,
+                                floatingLabelStyle: TextStyle(
+                                  fontSize: 25,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 20),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Password',
+                                hintText: 'Enter password here',
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 10),
+                            child: ElevatedButton(
+                              child: const Text('Coordinator Screen'),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Gscscreen()),
+                                );
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 10),
+                            child: ElevatedButton(
+                              child: const Text('Graduate Student Screen'),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const GradStudentscreen()),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+              ))),
+    );
   }
 }
