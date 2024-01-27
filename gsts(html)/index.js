90bfa50ce06be89fcd998863f2c2f194d65b077f
Michael Roxas <mroxas29@gmail.com>
2024-01-27 20:20:32 +0800

    test failed, too many changes

--- gsts(html)/index.js
+++ gsts(html)/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
 const app = express();
-const PORT = 8080;
 
-// Serve static files from the "assets" directory
-app.use(express.static(path.join(__dirname, "assets")));
+const admin = require("firebase-admin");
+/*const credentials = require("./serviceAccountKey.json");
 
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});*/
+app.post('/signup', async(req, res) => {
+  const userResponse =await admin.auth().createUser({
+    email: req.body.email,
+    password: req.body.password,
+    emailVerified: false,
+    disabled: false,
+  })
+  res.json(userResponse)
+})
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // Start the server
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
 });
